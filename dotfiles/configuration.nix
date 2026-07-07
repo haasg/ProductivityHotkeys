065ab94b30bb742ce30abb3030beb0d8c69226f6
haasg <griffin.haas1@gmail.com>
2026-07-07 15:31:44 -0700

    home manager

--- dotfiles/configuration.nix
+++ dotfiles/configuration.nix
@@ -8,5 +8,41 @@
   nixpkgs.hostPlatform = "aarch64-darwin"; # use x86_64-darwin for Intel CPU
 
   system.primaryUser = "haasg";
+  users.users.haasg = {
+    home = "/Users/haasg";
+  };
   system.stateVersion = 6;
+  system.defaults = {
+    NSGlobalDomain = {
+      AppleInterfaceStyle = "Dark";
+      KeyRepeat = 2;          # fast key repeat
+      InitialKeyRepeat = 15;  # short delay before repeat
+      _HIHideMenuBar = true;  # auto-hide the menu bar
+      AppleShowAllExtensions = true;
+    };
+    dock.autohide = true;
+    finder.FXPreferredViewStyle = "Nlsv";  # list view by default
+    finder.CreateDesktop = false;          # clean desktop
+    trackpad.Clicking = true;              # tap to click
+  };
+  nix-homebrew = {
+    enable = true;
+    user = "haasg";
+    autoMigrate = true;
+  };
+  homebrew = {
+    enable = true;
+    onActivation.cleanup = "zap";  # remove anything not listed here
+    onActivation.autoUpdate = true;
+    onActivation.extraFlags = [ "--force" ];
+    brews = [
+      "herdr"
+    ];
+    casks = [
+      "wezterm"
+      #"claude-code"
+      "adoptopenjdk8"
+      "corretto@11"
+    ];
+  };
 }
---
+++ dotfiles/home.nix
@@ -0,0 +1,46 @@
+{ config, pkgs, ... }:
+
+let
+  dotfiles = "${config.home.homeDirectory}/.dotfiles";
+in
+
+{
+  home.username = "haasg";
+  home.homeDirectory = "/Users/haasg";
+  home.stateVersion = "24.11";
+  home.packages = with pkgs; [
+    # cli i use constantly
+    ripgrep   # fast search
+    fd        # fast find
+    fzf       # fuzzy finder
+    jq        # json on the command line
+    lazygit
+    neovim
+    # the font everything renders in
+    nerd-fonts.hack
+  ];
+  fonts.fontconfig.enable = true;
+  home.sessionVariables.EDITOR = "nvim";
+
+  programs.zsh = {
+    enable = true;
+    autosuggestion.enable = true;      # ghost text from history
+    syntaxHighlighting.enable = true;  # commands turn green when valid
+    initContent = ''
+      bindkey '^f' autosuggest-accept
+    '';
+    shellAliases = {
+      ".." = "cd ..";
+      gst = "git status";
+      add = "git add .";
+      push = "git push";
+      pull = "git pull";
+      m = "git switch main";
+      cc = "claude";
+    };
+  };
+
+  # Edit-in-place: the real file stays in my repo, ~/.config just points at it.
+  home.file.".config/wezterm".source =
+    config.lib.file.mkOutOfStoreSymlink "${dotfiles}/home/.config/wezterm";
+}
