0ca09a88407ee20cc1e0329ff13da846c413a504
haasg <griffin.haas1@gmail.com>
2026-07-08 16:52:55 -0700

    change workspace hotkeys

--- dotfiles/home/.config/wezterm/wezterm.lua
+++ dotfiles/home/.config/wezterm/wezterm.lua
@@ -10,4 +10,28 @@ config.macos_window_background_blur = 50
 config.hide_tab_bar_if_only_one_tab = true
 config.window_decorations = "RESIZE"
 
+-- Cmd = navigate herdr workspaces (mirrors Ctrl = navigate agents).
+-- The Cmd/super modifier can't travel through a terminal input stream, so instead of
+-- forwarding Cmd itself we translate it into herdr's prefix sequence (Ctrl+Space, then Shift+H/L).
+-- This also overrides WezTerm's default Cmd+N (New Window) and macOS Cmd+H (Hide).
+local act = wezterm.action
+config.keys = {
+  {
+    key = "h",
+    mods = "CMD",
+    action = act.Multiple({
+      act.SendKey({ key = " ", mods = "CTRL" }), -- herdr prefix (Ctrl+Space)
+      act.SendKey({ key = "h", mods = "SHIFT" }), -- previous_workspace (prefix+shift+h)
+    }),
+  },
+  {
+    key = "n",
+    mods = "CMD",
+    action = act.Multiple({
+      act.SendKey({ key = " ", mods = "CTRL" }),
+      act.SendKey({ key = "l", mods = "SHIFT" }), -- next_workspace (prefix+shift+l)
+    }),
+  },
+}
+
 return config
